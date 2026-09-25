c6d1af917cc2eb97bcaef25d323c1640a2e0a0ff
vishal patel <patel.com6996@gmail.com>
2026-09-25 12:26:56 +0530

    Configure allowed hosts for deployment

--- resume_folder/settings.py
+++ resume_folder/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
 
 
 # Application definition
