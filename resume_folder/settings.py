f0cfd8f31a83354a2723e8f824349afd482cff17
vishal patel <patel.com6996@gmail.com>
2026-09-25 11:46:06 +0530

    Secure Django secret key

--- resume_folder/settings.py
+++ resume_folder/settings.py
@@ -1,3 +1,5 @@
+import os
+from pathlib import Path
 """
 Django settings for resume_folder project.
 
@@ -20,7 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yfh_+!s=%tz6d%w^vu!r(*@hj@=16dh#s1nc-&&k89@*qfzkx+'
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY',
+    'django-insecure-local-development-key'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
